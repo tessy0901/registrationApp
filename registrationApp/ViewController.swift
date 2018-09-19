49e8da2bed8873bdce79920d890468fab6d653bf
tessy0901 <tessy0901@gmail.com>
2018-09-20 00:02:09 +0900

    Initial Commit

---
+++ registrationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  registrationApp
+//
+//  Created by 手嶋慧太 on 2018/09/20.
+//  Copyright © 2018年 手嶋慧太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ registrationAppTests/registrationAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  registrationAppTests.swift
+//  registrationAppTests
+//
+//  Created by 手嶋慧太 on 2018/09/20.
+//  Copyright © 2018年 手嶋慧太. All rights reserved.
+//
+
+import XCTest
+@testable import registrationApp
+
+class registrationAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
