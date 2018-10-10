22a3ee18a6d6cc2a2298c910c689ce5b675bc275
tessy0901 <tessy0901@gmail.com>
2018-10-10 16:27:02 +0900

    view add

---
+++ registrationApp/AddProductViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddProductViewController.swift
+//  registrationApp
+//
+//  Created by 手嶋慧太 on 2018/10/10.
+//  Copyright © 2018年 手嶋慧太. All rights reserved.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
